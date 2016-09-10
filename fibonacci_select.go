6c752e67cea220dad94571586373f6d29e5cc68f
ken5scal <kengoscal@gmail.com>
2016-09-10 23:22:43 +0900

    Done finbonacci select

--- fibonacci_select.go
+++ fibonacci_select.go
@@ -7,11 +7,24 @@ func main() {
 	quit := make(chan int)
 
 	go func() {
-		for i:=0; i<10;i++ {
+		for i := 0; i < 10; i++ {
 			fmt.Println(<-c)
 		}
 		quit <- 0
 	}()
 
 	fibonacci3(c, quit)
+}
+
+func fibonacci3(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x + y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
 }
