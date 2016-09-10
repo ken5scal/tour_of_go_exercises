6e7ecf74d0f0c4107fd59d1b663b8e3a99767b2d
ken5scal <kengoscal@gmail.com>
2016-09-10 23:19:43 +0900

    Implement main function

--- fibonacci_select.go
+++ fibonacci_select.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+
+	go func() {
+		for i:=0; i<10;i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+
+	fibonacci3(c, quit)
 }
