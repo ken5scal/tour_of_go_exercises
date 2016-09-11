548b2a8ecf0442e78386d8984cec5e43f25e08de
ken5scal <kengoscal@gmail.com>
2016-09-11 14:15:20 +0900

    Add

--- binary_tree_check.go
+++ binary_tree_check.go
@@ -4,14 +4,18 @@ import "fmt"
 import "golang.org/x/tour/tree"
 
 func main() {
-	c := make(chan int, 10)
-	go Walk(tree.New(1), c)
 
 	//for i := 0; i < cap(c); i++ {
 	//	fmt.Println(<-c)
 	//}
-	result := Same(tree.New(1), tree.New(2))
-	fmt.Println(result)
+	t1 := tree.New(1)
+	t2 := tree.New(2)
+
+	fmt.Println(t1.String())
+	fmt.Println(t2.String())
+
+	fmt.Println(Same(t1, t2))
+	fmt.Println(Same(t1, t1))
 }
 
 func Walk(tree *tree.Tree, c chan int) {
@@ -26,5 +30,19 @@ func Walk(tree *tree.Tree, c chan int) {
 }
 
 func Same(t1 *tree.Tree, t2 *tree.Tree) bool {
+	c := make(chan int, 10)
+
+	go Walk(t1, c)
+	go Walk(t2, c)
+
+	for i:= 0; i < cap(c); i++ {
+		t1_v, t2_v := <-c, <-c
+		fmt.Printf("t1_v: %d\n", t1_v)
+		fmt.Printf("t2_v: %d\n", t2_v)
+		if t1_v != t2_v {
+			return false
+		}
+	}
+
 	return true
 }
