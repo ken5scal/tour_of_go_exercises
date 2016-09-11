ecb1aa75415196aece43112c4b94b568e127f31e
ken5scal <kengoscal@gmail.com>
2016-09-11 14:27:44 +0900

    rename variables

--- binary_tree_check.go
+++ binary_tree_check.go
@@ -11,31 +11,31 @@ func main() {
 	fmt.Println(Same(t1, t1))
 }
 
-func Walk(tree *tree.Tree, c chan int) {
-	_walk(tree, c)
-	close(c)
+func Walk(tree *tree.Tree, ch chan int) {
+	_walk(tree, ch)
+	close(ch)
 }
 
-func _walk(tree *tree.Tree, c chan int) {
+func _walk(tree *tree.Tree, ch chan int) {
 	if (tree.Left != nil) {
-		_walk(tree.Left, c)
+		_walk(tree.Left, ch)
 	}
 	if (tree.Right != nil) {
-		_walk(tree.Right, c)
+		_walk(tree.Right, ch)
 	}
 
-	c <- tree.Value
+	ch <- tree.Value
 }
 
 func Same(t1 *tree.Tree, t2 *tree.Tree) bool {
-	c1 := make(chan int)
-	c2 := make(chan int)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 
-	go Walk(t1, c1)
-	go Walk(t2, c2)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
 
-	for i:= range c1 {
-		if i != <-c2 {
+	for i:= range ch1 {
+		if i != <-ch2 {
 			return false
 		}
 	}
