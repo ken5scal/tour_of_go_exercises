1afd167a42b20619b5f0c708b424a378727cb904
ken5scal <kengoscal@gmail.com>
2016-09-11 13:43:26 +0900

    Move close(channel) to sender

--- binary_tree_check.go
+++ binary_tree_check.go
@@ -4,8 +4,17 @@ import "fmt"
 import "golang.org/x/tour/tree"
 
 func main() {
-	ch := make(chan int)
+	ch := make(chan int, 10)
 	go Walk(tree.New(1), ch)
+
+	for i := range ch {
+		fmt.Println(i)
+	}
 }
 
-func Walk(tree *tree.Tree, c chan int) {}
+func Walk(tree *tree.Tree, c chan int) {
+	for i := 0; i < cap(c); i++ {
+		c <- i + 1
+	}
+	close(c)
+}
