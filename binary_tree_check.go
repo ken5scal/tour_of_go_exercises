6ee42fc9f19a96d0c697c664e9a4bb833cd4158d
ken5scal <kengoscal@gmail.com>
2016-09-11 14:07:19 +0900

    Add recursive walk

--- binary_tree_check.go
+++ binary_tree_check.go
@@ -7,16 +7,20 @@ func main() {
 	c := make(chan int, 10)
 	go Walk(tree.New(1), c)
 
-	for i := range c {
-		fmt.Println(i)
+	for i := 0; i < cap(c); i++ {
+		fmt.Println(<-c)
 	}
 }
 
 func Walk(tree *tree.Tree, c chan int) {
-	for i := 0; i < cap(c); i++ {
-		c <- i + 1
+	if (tree.Left != nil) {
+		Walk(tree.Left, c)
 	}
-	close(c)
+	if (tree.Right != nil) {
+		Walk(tree.Right, c)
+	}
+
+	c <- tree.Value
 }
 
 func Same(t1 *tree.Tree, t2 *tree.Tree) bool {
