a818d7457d114c8e1f77f3deeb8cdbc2690acfab
ken5scal <kengoscal@gmail.com>
2016-09-18 23:34:03 +0900

    Add fetched structure

--- web_crawler.go
+++ web_crawler.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Fetcher interface {
 	// Fetch returns the body of URL and
@@ -30,10 +33,6 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	return
 }
 
-func main() {
-	Crawl("http://golang.org/", 4, fetcher)
-}
-
 //fkeFetcher is Fetcher that returns canned results
 type fakeFetcher map[string]*fakeResult
 
@@ -43,6 +42,7 @@ type fakeResult struct {
 }
 
 func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+
 	if res, ok := f[url]; ok {
 		return res.body, res.urls, nil
 	}
@@ -58,27 +58,27 @@ var fetcher = fakeFetcher{
 			"http://golang.org/cmd/",
 		},
 	},
-	"http://golang.org/pkg/": &fakeResult{
-		"Packages",
-		[]string{
-			"http://golang.org/",
-			"http://golang.org/cmd/",
-			"http://golang.org/pkg/fmt/",
-			"http://golang.org/pkg/os/",
-		},
-	},
-	"http://golang.org/pkg/fmt/": &fakeResult{
-		"Package fmt",
-		[]string{
-			"http://golang.org/",
-			"http://golang.org/pkg/",
-		},
-	},
-	"http://golang.org/pkg/os/": &fakeResult{
-		"Package os",
-		[]string{
-			"http://golang.org/",
-			"http://golang.org/pkg/",
-		},
-	},
+	//"http://golang.org/pkg/": &fakeResult{
+	//	"Packages",
+	//	[]string{
+	//		"http://golang.org/",
+	//		"http://golang.org/cmd/",
+	//		"http://golang.org/pkg/fmt/",
+	//		"http://golang.org/pkg/os/",
+	//	},
+	//},
+	//"http://golang.org/pkg/fmt/": &fakeResult{
+	//	"Package fmt",
+	//	[]string{
+	//		"http://golang.org/",
+	//		"http://golang.org/pkg/",
+	//	},
+	//},
+	//"http://golang.org/pkg/os/": &fakeResult{
+	//	"Package os",
+	//	[]string{
+	//		"http://golang.org/",
+	//		"http://golang.org/pkg/",
+	//	},
+	//},
 }
